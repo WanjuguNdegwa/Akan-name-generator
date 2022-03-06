67cf892cb3f6e1769ace13f167f26efece7f3556
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-03-06 20:42:02 +0300

    Create arrays to store the male and female names

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 function getAkanName() {
     const birthday = document.getElementById("birthday").value;
     const date = new Date(birthday);
-    console.log(birthday)
+    const dayOfWeek = date.getDay();
+
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+    console.log(dayOfWeek);
 }
